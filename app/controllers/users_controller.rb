70d38e87f527c2014f885384983acb991e86f00a
Dustin Milam <darklordsushi@yahoo.com>
2015-10-06 13:34:33 -0400

    add create/update meathods and form partial to new

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,13 +13,29 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new()
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:success] = "Welcome to the Pokedex!"
+      redirect_to @user
+    else
+      render @user
+    end
+
   end
 
   def edit
   end
 
   def update
+    @user = User.find(params[:id])
+
+    if @user.update_attributes(user_params)
+      flash[:notice] = "Successfully updated..."
+      redirect_to edit_user_path
+    else
+      render :show
+    end
   end
 
   def destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
 	serialize :minesweeper_scores, Array
+	validates :first_name, presence: true
+	validates :last_name, presence: true
+	validates :email, presence: true
+	validates :password_digest, presence: true
 
 	def full_name
 		first_name + " " + last_name
